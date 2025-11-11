529ec60b02ac0b30536f747e07fc97de4b4fa5c2
Adrien Ferrand <ferrand.ad@gmail.com>
2025-11-11 23:29:05 +0100

    Cleanup

--- tests/providers/test_desec.py
+++ tests/providers/test_desec.py
@@ -1,12 +1,11 @@
 """Integration tests for deSEC"""
-from unittest import TestCase
 from integration_tests import IntegrationTestsV2
 
 
 # Hook into testing framework by inheriting unittest.TestCase and reuse
 # the tests which *each and every* implementation of the interface must
 # pass, by inheritance from integration_tests.IntegrationTestsV2
-class DesecProviderTests(TestCase, IntegrationTestsV2):
+class DesecProviderTests(IntegrationTestsV2):
     """Integration tests for deSEC provider"""
 
     provider_name = "desec"
