cb4d9d2997418f9768d2d09e2d509d279ade5210
Adrien Ferrand <ferrand.ad@gmail.com>
2025-11-11 17:20:31 +0100

    Ensure to load the tld cache before running tests

--- tests/providers/test_godaddy.py
+++ tests/providers/test_godaddy.py
@@ -2,13 +2,8 @@
 
 from unittest import TestCase
 
-import pytest
-
 from integration_tests import IntegrationTestsV2
 
-
-# TODO: Refresh these cassettes do re-enable the tests.
-@pytest.mark.skip("Cassettes need to be refreshed")
 class GoDaddyProviderTests(TestCase, IntegrationTestsV2):
     """TestCase for Godaddy"""
 
